076aa90a58d692c585772222f3a5cc81aa6a9540
Ayush Bommana <bommanaayush07@gmail.com>
2025-12-22 13:43:37 +0530

    04: Setup BG job with inngest

--- src/modules/trpc/routers/_app.ts
+++ src/modules/trpc/routers/_app.ts
@@ -1,16 +1,32 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { z } from "zod";
+import { baseProcedure, createTRPCRouter } from "../init";
+import { inngest } from "@/modules/inngest/client";
 export const appRouter = createTRPCRouter({
   hello: baseProcedure
     .input(
       z.object({
         text: z.string(),
-      }),
+      })
     )
     .query((opts) => {
       return {
         greeting: `Namaste ${opts.input.text} paaji`,
       };
+    }),
+
+  invokeBG: baseProcedure
+    .input(
+      z.object({
+        name: z.string(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      await inngest.send({
+        name: "test/hello.world",
+        data: {
+          name: input?.name,
+        },
+      });
     }),
 });
 // export type definition of API
