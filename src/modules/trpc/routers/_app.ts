5c93d7ffc26249cdabee6898697c608e7c71b9c0
Ayush Bommana <bommanaayush07@gmail.com>
2025-12-23 18:03:56 +0530

    08: Setup Messages & Fragment DB

--- src/modules/trpc/routers/_app.ts
+++ src/modules/trpc/routers/_app.ts
@@ -1,33 +1,9 @@
 import { z } from "zod";
 import { baseProcedure, createTRPCRouter } from "../init";
 import { inngest } from "@/modules/inngest/client";
+import { messageRouter } from "@/modules/messages/procedures";
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
-    .query((opts) => {
-      return {
-        greeting: `Namaste ${opts.input.text} paaji`,
-      };
-    }),
-
-  invokeBG: baseProcedure
-    .input(
-      z.object({
-        prompt: z.string(),
-      })
-    )
-    .mutation(async ({ input }) => {
-      await inngest.send({
-        name: "test/hello.world",
-        data: {
-          prompt: input?.prompt,
-        },
-      });
-    }),
+    messages: messageRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
